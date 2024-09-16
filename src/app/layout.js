c8adcc390307cc7caa7269030204338763c0982a
Solmoon21 <rpob716@gmail.com>
2024-09-16 13:25:44 +0200

    Icon Reupload

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,8 +17,9 @@ export default function RootLayout({ children }) {
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        ></meta>
+        />
         <link rel="manifest" href="manifest.json" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
       </head>
       <body className={inter.className}>
         <Navbar />
